ed0064b6fa2766949f002d91af79a54708bd07cc
DACUS1995 <studormarian@yahoo.co.uk>
2018-12-26 00:49:31 +0200

    get intermediate layers

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ import utils
 
 tf.enable_eager_execution()
 
+STYLE_LAYERS = [
+	"block1_conv1",
+	"block2_conv1",
+	"block3_conv1", 
+	"block4_conv1", 
+	"block5_conv1"
+]
+
+CONTENT_LAYERS = [
+	"block5_conv2"
+]
+
 
 def get_intermediate_layers(layers: Dict) -> models.Model:
 	vgg_model = tf.keras.applications.VGG19(include_top=False, weights="imagenet")
@@ -20,8 +32,8 @@ def get_intermediate_layers(layers: Dict) -> models.Model:
 	return models.Model(vgg_model.input, all_outputs)
 
 
-def compute_style_loss():
-	pass
+def compute_style_loss(tranformed_image, target_content_image):
+	return tf.reduce_mean(tf.square(tranformed_image - target_content_image))
 
 
 def compute_content_loss():
@@ -29,7 +41,10 @@ def compute_content_loss():
 
 
 def transfer_style(content_image, style_image):
-	pass
+	model = get_intermediate_layers({
+		"style_layers": STYLE_LAYERS,
+		"content_layers": CONTENT_LAYERS
+	})
 
 
 
@@ -43,9 +58,7 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser("Style Transfer")
 	parser.add_argument("-l", "--learning-rate", type=int, default=0.05, help="learning rate")
 	parser.add_argument("-s", "--style-image-path", type=str, default="./images/style/style.jpg", help="path to the style images")
-	parser.add_argument("-c", "--content-image-path", type=str, default=".
-	
-	as/images/content/content.jpg", help="path to the content images")
+	parser.add_argument("-c", "--content-image-path", type=str, default="./images/content/content.jpg", help="path to the content images")
 	args = parser.parse_args()
 
 	main(args)
